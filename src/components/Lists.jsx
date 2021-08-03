1604b882156ad9aa6df82ab5677dbae10990b132
Alli-Oops <AllisonLeonardSeattle@gmail.com>
2021-08-03 11:34:37 -0700

    created the database in test mode - then added a lists collection. Created a Lists.jsx component that displays lists that the user creates dynamically at the bottom of the authenticated user's homepage

---
+++ src/components/Lists.jsx
@@ -0,0 +1,91 @@
+//This List.jsx component will display a list(s) that a user has created at the bottom of the Authenticated-User's Homepage
+
+import React from "react";
+import { Link } from "react-router-dom";
+import defaultImage from "../../static/default.svg";
+import * as db from "../firestore";
+import Empty from "./shared/Empty";
+import Error from "./shared/Error";
+import Loading from "./shared/Loading";
+import useSWR from 'swr' // stale while revalidate 
+
+
+function UserLists({ user }) {
+    const { data: lists, error } = useSWR(user.uid, db.getUserLists)    // useSWR() accepts first argument - in this case the 'str' that is the userID. // And that first argument is passed to the second argument. // The second argument is a function that takes care of the fetching for us - so in this case, the db.getUserLists function // from useSWR(user.uid, db.getUserLists) ... we get back an object {} that has a couple of values { data , error } // data comes back if it resolves successfully OR error comes back if there is a problem with performing the request.
+    if (error) return <Error message={error.message} />;                // If there is an error, we want to return the error component
+    if (!lists) return <Loading/>;                                      // If we dont have our list data yet, we want to return our Loading.jsx component
+    if (lists.length === 0) return <Empty />;                           // if we are able to fetch the data and there's no error, but the list array is empty meaning lists.length === 0) // we can return the empty component - which just prompts the user to add a list.
+    return (
+    <>
+    {/* display user list count */}
+      <UserListCount count={lists.length}/>                             {/* <<here>> we provide a count prop to the UserListCOunt component */}
+    <section className="text-gray-500 bg-gray-900 body-font">
+        <div className="container px-5 py-5 mx-auto">
+            <div className="flex flex-wrap -m-4">
+                {/* display lists that user is part of  */}
+                {lists.map(list => (                                    // as we map over each list, we can use the ListItem function from *below*
+                    <ListItem key={list.id} list={list}/>               // provide the key as {list.id} and pass all the list data down on a prop named list={list}
+                ))} 
+            </div>
+        </div>
+    </section>
+    </>
+    );
+}
+
+function UserListCount({ count }) {                                     // we can destructure *count* and interpolate it in the <h2> div below
+    return (
+        <div className="container px-5 py-5 mb-6 bg-gray-800 rounded mx-auto flex justify-center text-center">
+        <div className="p-4 sm:w-1/4 w-1/2">
+            <svg
+            fill="none"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            className="text-green-500 w-12 h-12 mb-3 inline-block"
+            viewBox="0 0 24 24"
+            >
+            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <h2 className="title-font font-medium sm:text-4xl text-3xl text-white">
+            {count}                                                     {/* this is where we interpolate count. And then to display the lists that the user is part of - we do that in the ListItem()*/}
+            </h2>
+            <p className="leading-relaxed">Lists</p>
+        </div>
+        </div>
+    );
+}
+
+function ListItem({ list }) {                                           // this takes the data from the list prop to display it to the user's lists - but we can destructure the list object
+  const { id, name, description, image} = list                          // this destructures the list object to get the values we need - id, name, description, image, and users that are a part of the list
+    return (
+        <div className="lg:w-1/3 sm:w-1/2 p-4">
+        {" "}
+        <Link to={`/id`}>                                               {/* the id that we get from destructuring the list is for this Link - which links various users to the list*/}
+            <div className="flex relative">
+            <img
+                alt="gallery"
+                className="absolute inset-0 w-full h-full object-cover object-center"
+                src={image || defaultImage}                             {/* here we display either the image provided or the defaultImage */}
+            />
+            <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
+                <ul className="list-disc">
+                <li className="tracking-widest text-sm title-font font-medium text-orange-500 mb-1">
+                    {users[0].name} {users.length > 1 && `+ ${users.length - 1} others`}
+                                                                        {/* Here ^^ we display the users Username - and that comes from the user's array*/}
+                                                                        {/* Then we we can use a terenary to display if other the users are apart of the list */}
+                </li>
+                </ul>
+                <h1 className="title-font text-lg font-medium text-white mb-3">
+                {name}                                                  {/* This includes the name of the list from *name */}
+                </h1>
+                <p className="leading-relaxed">{description}</p>        {/* This includes the description of the list from *description property */}
+            </div>
+            </div>
+        </Link>
+        </div>
+    );
+}
+
+export default UserLists;
--- src/firestore/index.js
+++ src/firestore/index.js
@@ -5,6 +5,7 @@ import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
 
+//////////////////////////////////////////////////////////////////////////
 /* ######### Integrates Firebase with existing FireStinksApp ########## */
 var firebaseConfig = {
     apiKey: "AIzaSyAfP2G73bTgAoGJg-dQUTJBgLRN6jeVDeI",
@@ -19,22 +20,58 @@ var firebaseConfig = {
 const firebaseApp = !firebase.apps.length
     ? firebase.initializeApp(firebaseConfig)
     : firebase.app();
-const db = firebaseApp.firestore();                                 // reference to the firestore database
-const auth = firebaseApp.auth();                                    // reference to the auth module
-const storage = firebaseApp.storage();                              // reference to the storage module
+const db = firebaseApp.firestore();                                             // reference to the firestore database
+const auth = firebaseApp.auth();                                                // reference to the auth module
+const storage = firebaseApp.storage();                                          // reference to the storage module
 
+////////////////////////////////////////////////////////////////////////////////
 /* ######### Export Functions for Sign-In, Authentication, Log-Out ########## */
 export async function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider()         // "firebase.auth" is not referencing this as a method but rather, as a property and then we want to access the GoogleAuthProvider() // we store our reference to that in our "provider" variable
-    await auth.signInWithPopup(provider)                            // we want to await it because this method returns a promise **Not going to resolve immeditaely but after the user completes the signup flow
-    window.location.reload()                                        // after signing in with the popup, we can reload the page with window.location.reload()   -- this lets us redirect away from the "Splash" Page and actually show the authenticated content.
+    const provider = new firebase.auth.GoogleAuthProvider()                     // "firebase.auth" is not referencing this as a method but rather, as a property and then we want to access the GoogleAuthProvider() // we store our reference to that in our "provider" variable
+    await auth.signInWithPopup(provider)                                        // we want to await it because this method returns a promise **Not going to resolve immeditaely but after the user completes the signup flow
+    window.location.reload()                                                    // after signing in with the popup, we can reload the page with window.location.reload()   -- this lets us redirect away from the "Splash" Page and actually show the authenticated content.
 }     
 
 export function checkAuth(cb) {
-    return auth.onAuthStateChanged(cb);                             // << Here >> within a callback function, we can get access to the user.  
+    return auth.onAuthStateChanged(cb);                                         // << Here >> within a callback function, we can get access to the user.  
 }
 
 export async function logOut() {
-    await auth.signOut();                                           // this returns a promise and then after doing so, will reload the page
-    window.location.reload();                                       // we want to use this function when we hit the logout button in the NavBar component
+    await auth.signOut();                                                       // this returns a promise and then after doing so, will reload the page
+    window.location.reload();                                                   // we want to use this function when we hit the logout button in the NavBar component
 }
+
+/////////////////////////////////////////////////////////////////////////
+/* ######### retrieve/GET the User List Data from Firestore ########## */       // we pass in the collection with "id" which is going to be the 'lists' collection
+export async function getCollection(id) {                                       // to get all the data we need to reference the collection and use the get()
+    const snapshot = await db.collection(id).get()                              // it will be in the form of a promise, so we need to make the function async and await the response // we will get back a query "snapshot" so we'll call the variable holding this "snapshot"
+    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data()}))       // then all of the data- will be provided on snapshot.docs // then we use the map() function to map over it and for each document, we want to create a new array where we return an object
+    console.log(data)                                                           // so an array of objects where we set the id to the document id, and then use the method data() and ... spread in all the fields we're getting from the database into this object
+}
+
+                                                                                // Then - query collection data from our lists collection // getUserLists(userId) function iss specific for getting a specific user's lists // how we use the data (once it's fetched) is in the Lists.jsx component
+export async function getUserLists(userId) {                                    // we pass in the user id with "userId" 
+    const snapshot = await db
+    .collection('lists')                                                        // we reference the collection called lists 
+    .where('author', '==', userId)                                              // then use the where() to say lists where the author is equal to the 'userID'
+    .get();
+    const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data()})) ;
+    return data
+}
+
+function uploadCoverImage(file) {                                                                                // The uploadCoverImage function lets us upload the image tot he database/ It accepts the user's file // And to use storage, we have a reference to that (towards the top of this file -- const storage = firebaseApp.storage();)
+    const uploadTask = storage
+        .ref(`images/${file.name}-${file.lastModified}`)                                                         // this `images/${file.name}` images/ creates a folder called images, that we put in the ${} location - giving the file a name with "file.name" (note: there is a name property for the image when the user uploads it)
+        .put(file)                                                                                               // then we pass the file to the put method
+    return new Promise((resolve, reject) => {                                                                    // for each promise, we get the resolve and reject functions.. and pass all the code for uploadTask.on() into the promise
+        uploadTask.on(                                                                                           // << we can listen to the progress of this task with uploadTask.on() and give it the first argument as the string "state_changed"
+            'state_changed',                                                    
+            (snapshot) => console.log("image uploading", snapshot),
+            reject,                                                                                              // reject <<here>> and then we can resolve the promise in the *.then() 
+            () => {
+                storage.ref('images').child(`${file.name}-${file.lastModified}`).getDownloadURL().then(resolve)  // THen, << the callback where we can get the URL and the URL comes from: storage.ref() where we pass in images. We need to resolve this as a promise with .then()
+            }                                                                                                    // Since we need to pass the URL to the creatList() function *below* we need to promisify the uploadCoverImage() function.. // by returning a promise, the createList() function can easily resolve it with the *async* *await* syntax.
+        );                                                                                                       // We promisify the the uploadCoverImage(file) function with the line new Promise()        
+    })
+}
+
